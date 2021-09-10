6584a315d3433c94810e9b879501c5b456377a0b
marcelopoars <marcelo.fatecpoa@gmail.com>
2021-09-09 21:42:17 -0300

    Refactoring - exercícios 01 e 02

--- exercicios/js/ex-01.js
+++ exercicios/js/ex-01.js
@@ -1,34 +1,34 @@
+console.log("*** Exercício 01 - Séries Inválidas ***");
+
 function invalidas() {
   const anoAtual = new Date().getFullYear();
 
-  const seriesInvalidas = series.filter(
-    ({
-      titulo,
-      anoEstreia,
-      diretor,
-      genero,
-      elenco,
-      temporadas,
-      numeroEpisodios,
-      distribuidora,
-    }) =>
-      anoEstreia > anoAtual ||
-      !titulo ||
-      !anoEstreia ||
-      !diretor ||
-      !genero ||
-      !elenco ||
-      !temporadas ||
-      !numeroEpisodios ||
-      !distribuidora
-  );
-
-  const result = seriesInvalidas
+  const seriesInvalidas = series
+    .filter(
+      ({
+        titulo,
+        anoEstreia,
+        diretor,
+        genero,
+        elenco,
+        temporadas,
+        numeroEpisodios,
+        distribuidora,
+      }) =>
+        anoEstreia > anoAtual ||
+        !titulo ||
+        !anoEstreia ||
+        !diretor ||
+        !genero ||
+        !elenco ||
+        !temporadas ||
+        !numeroEpisodios ||
+        !distribuidora
+    )
     .map(({ titulo }) => (titulo ? titulo : "Campo nome vazio"))
     .join(" - ");
 
-  return `Séries Inválidas: ${result}`;
+  return `Séries Inválidas: ${seriesInvalidas}`;
 }
 
-console.log("*** Exercício 01 - Séries Inválidas ***");
 console.log(invalidas());
--- exercicios/js/ex-02.js
+++ exercicios/js/ex-02.js
@@ -1,7 +1,7 @@
-function filtrarPorAno(ano) {
-  const result = series.filter(({anoEstreia}) => anoEstreia >= ano);
-  return result
+console.log("*** Exercício 02 - Séries a partir de um determinado ano ***");
+
+function filtrarPorAno(anoEscolhido) {
+  return series.filter(({ anoEstreia }) => anoEstreia >= anoEscolhido);
 }
 
-console.log("*** Exercício 02 - Séries a partir de um determinado ano ***");
 console.table(filtrarPorAno(2017));

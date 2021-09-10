f9ff9a5cba4c4356f56a450627a63a25b5e58c64
marcelopoars <marcelo.fatecpoa@gmail.com>
2021-09-09 23:39:59 -0300

    Refactoring - exercícios 04 e 05

--- exercicios/js/ex-04.js
+++ exercicios/js/ex-04.js
@@ -2,7 +2,8 @@ console.log("*** Exercício 04 - Média de Episódios ***");
 
 function mediaDeEpisodios() {
   const media = series.reduce(
-    (total, serie, _, array) => total + serie.numeroEpisodios / array.length,
+    (total, { numeroEpisodios }, _, array) =>
+      total + numeroEpisodios / array.length,
     0
   );
   return media;
--- exercicios/js/ex-05.js
+++ exercicios/js/ex-05.js
@@ -1,3 +1,5 @@
+console.log("*** Exercício 05 - Mascada em Série ***");
+
 function formatarMoeda(valor) {
   const result = valor.toLocaleString("pt-br", {
     style: "currency",
@@ -11,18 +13,13 @@ function totalSalarios(indiceDaSerie) {
   const salarioDiretor = 100000;
   const salarioElenco = 40000;
 
-  const totalSalarioDiretor =
-    salarioDiretor * series[indiceDaSerie].diretor.length;
+  const { diretor, elenco } = series[indiceDaSerie];
 
-  const totalSalarioElenco =
-    salarioElenco * series[indiceDaSerie].elenco.length;
+  const totalSalarioDiretor = diretor.length * salarioDiretor
+  const totalSalarioElenco = elenco.length * salarioElenco
 
-  let result = totalSalarioDiretor + totalSalarioElenco;
+  return formatarMoeda(totalSalarioDiretor + totalSalarioElenco)
 
-  result = formatarMoeda(result);
-
-  return result;
 }
 
-console.log("*** Exercício 05 - Mascada em Série ***");
-console.log(totalSalarios(1));
+console.log(totalSalarios(0));

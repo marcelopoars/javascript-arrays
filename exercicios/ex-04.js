92026461f24d828f24536391c6ab8e7b78ca92ca
marcelopoars <marcelo.fatecpoa@gmail.com>
2021-09-11 18:46:50 -0300

    Refactoring exercício 04

--- exercicios/ex-04.js
+++ exercicios/ex-04.js
@@ -2,14 +2,11 @@ const series = require("./series");
 
 console.log("*** Exercício 04 - Média de Episódios ***");
 
-function mediaDeEpisodios() {
-  return (
-    series
-      .map(({ numeroEpisodios }) => numeroEpisodios)
-      .reduce(
-        (totalEpisodios, numeroEpisodios) => totalEpisodios + numeroEpisodios
-      ) / series.length
-  );
-}
+const mediaDeEpisodios = () =>
+  series
+    .map(({ numeroEpisodios }) => numeroEpisodios)
+    .reduce(
+      (totalEpisodios, numeroEpisodios) => totalEpisodios + numeroEpisodios
+    ) / series.length;
 
 console.log(mediaDeEpisodios());

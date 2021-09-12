d6ab7b87ad91d3319a6e71d377ccf9ff8405238f
marcelopoars <marcelo.fatecpoa@gmail.com>
2021-09-12 11:11:00 -0300

    Refactoring exercício 06

--- exercicios/ex-06.js
+++ exercicios/ex-06.js
@@ -2,10 +2,14 @@ const series = require("./series");
 
 console.log("*** Exercício 06 - A - Buscas! ***");
 
-const queroGenero = (generoDigitado) =>
-  series
+// const showSeries = (arr) => arr.map((s) => s.titulo);
+
+function queroGenero(generoDigitado) {
+  const result = series
     .filter(({ genero }) => genero.includes(generoDigitado))
     .map((serie) => serie.titulo);
+  return result;
+}
 
 console.log(queroGenero("Caos"));
 
@@ -14,8 +18,18 @@ console.log(queroGenero("Caos"));
 console.log("*** Exercício 06 - B - Buscas! ***");
 
 const queroTitulo = (palavraDigitada) =>
-  series
-    .filter(({ titulo }) => titulo.indexOf(palavraDigitada) !== -1)
-    .map((serie) => serie.titulo);
+  showSeriesDeclaration(
+    series.filter(({ titulo }) => titulo.indexOf(palavraDigitada) !== -1)
+  );
 
 console.log(queroTitulo("The"));
+
+// Function Statement
+const showSeriesStatement = (arr) => arr.map((s) => s.titulo);
+// ReferenceError: Cannot access 'showSeriesStatement' before initialization at queroTitulo
+
+// Function Declaration
+function showSeriesDeclaration(arr) {
+  return arr.map((s) => s.titulo);
+  // Aqui ocorre o Hoisting. Ver em https://developer.mozilla.org/pt-BR/docs/Glossary/Hoisting
+}
